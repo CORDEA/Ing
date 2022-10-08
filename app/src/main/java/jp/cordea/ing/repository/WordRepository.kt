2b871434694ac7bf417ce562c9b67bb13e6efdc6
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-08 13:41:04 +0900

    fetch words

--- app/src/main/java/jp/cordea/ing/repository/WordRepository.kt
+++ app/src/main/java/jp/cordea/ing/repository/WordRepository.kt
@@ -1,12 +1,15 @@
 package jp.cordea.ing.repository
 
 import jp.cordea.ing.Word
+import jp.cordea.ing.api.WordApi
+import jp.cordea.ing.api.WordRequest
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WordRepository @Inject constructor() {
-    suspend fun findAll(): List<Word> {
-        return emptyList()
-    }
+class WordRepository @Inject constructor(
+    private val api: WordApi
+) {
+    suspend fun findAll(): List<Word> =
+        api.getWords(WordRequest("doGet")).response.result
 }
