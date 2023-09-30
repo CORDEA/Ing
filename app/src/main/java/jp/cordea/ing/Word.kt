7fb8f20d8f19eaf2cd582e458071679fd38a65ca
Yoshihiro Tanaka <contact@cordea.jp>
2023-09-30 15:25:53 +0900

    add a category

--- app/src/main/java/jp/cordea/ing/Word.kt
+++ app/src/main/java/jp/cordea/ing/Word.kt
@@ -1,10 +1,22 @@
 package jp.cordea.ing
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Word(
     val id: Int,
     val question: String,
-    val answer: String
+    val answer: String,
+    val category: WordCategory
 )
+
+@Serializable
+enum class WordCategory {
+    @SerialName("word_book")
+    WORD_BOOK,
+    @SerialName("game")
+    GAME,
+    @SerialName("novel")
+    NOVEL
+}
